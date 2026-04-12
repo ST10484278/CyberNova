66a7c90a2f5df2960cbf68c7235536b4ab985f11
Tmk_Git <ST10484278@rcconnect.edu.za>
2026-04-12 19:27:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Media;
+using System.IO;
+namespace CyberNova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CyberNovaBot bot = new CyberNovaBot();
+
+          
+            Console.WriteLine("Please enter your name to begin the conversation.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " +userName+"! Welcome to cybersecurity chatbot.");
+    
+
+            
+            Console.WriteLine("Type 'help' for topics.");
+            Console.WriteLine("Type 'exit' to quit.");
+
+            while (true)
+            {
+                Console.Write("\nYou: ");
+                string userInput = Console.ReadLine();
+
+                string response = bot.GetResponse(userInput);
+
+                Console.WriteLine("CyberNova: " + response);
+
+                if (userInput.ToLower() == "exit")
+                    break;
+            }
+        }
+    }
+}
