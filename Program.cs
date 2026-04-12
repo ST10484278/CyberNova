765dc85726cf89f5d64737e7b983152068bef5ae
Tmk_Git <ST10484278@rcconnect.edu.za>
2026-04-12 19:38:48 +0200

    Added an  ASCII art.

--- Program.cs
+++ Program.cs
@@ -34,7 +34,15 @@ namespace CyberNova
                 Console.WriteLine("Audio file could not be played.");
                 Console.WriteLine(ex.Message);
             }
+            Console.WriteLine("========================================================================");
+            Console.WriteLine(@"_________       ______             _____   ________               
+__  ____/____  ____  /________________  | / /_  __ \__   _______ _
+_  /    __  / / /_  __ \  _ \_  ___/_   |/ /_  / / /_ | / /  __ `/
+/ /___  _  /_/ /_  /_/ /  __/  /   _  /|  / / /_/ /__ |/ // /_/ / 
+\____/  _\__, / /_.___/\___//_/    /_/ |_/  \____/ _____/ \__,_/  
+        /____/                                                    ");
 
+            Console.WriteLine("========================================================================");
             Console.WriteLine("Please enter your name to begin the conversation.");
             string userName = Console.ReadLine();
             Console.WriteLine("Hello " + userName + "! Welcome to cybersecurity chatbot.");
